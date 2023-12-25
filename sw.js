d15855f889b04f0e1f24f596385f53f49ed51fe7
Semidoggie <2792989766@qq.com>
2023-12-25 14:36:00 +0800

    Site updated: 2023-12-25 14:36:00

--- sw.js
+++ sw.js
@@ -1,1 +1,271 @@
-"use strict";var precacheConfig=[["/2022/10/30/Test/index.html","a8a3c32bf4401fda3397e20b3faa6451"],["/2022/10/30/hello-world/index.html","6c51e72775f1a816da36ce7116ea7933"],["/2022/12/16/Life/index.html","76dc0a30c133badbd366b83d3491a720"],["/2023/04/02/Demo-1/index.html","303582c1fe3fa9c53b35ac55ffe43337"],["/2023/04/18/Demo-2/index.html","29b064d29ca5e06c68af8c7f62bab24c"],["/2023/08/22/23电赛B题/index.html","bcf01e8aa8e4bdee3ebfbe2e694a449a"],["/404.html","ded38cf7b0ca3eed8a05a726fa8570f5"],["/about/index-1.html","21298da20ca4acc3edd3d9aae4c3c002"],["/about/index.html","f577859b87a88c7b11ca8769213b9254"],["/archives/2022/10/index.html","2e08840f7db49613137eb7dbaaf57cf4"],["/archives/2022/12/index.html","ba907ae7d8420079a5d2ba07d0255b49"],["/archives/2022/index.html","28a1acc7ba7ee33e232474b8f548935a"],["/archives/2023/04/index.html","a819a445e3558fd5d22bec5837f4e671"],["/archives/2023/08/index.html","1de8d1f6bd6f050fac7774cfe0186a55"],["/archives/2023/index.html","7b46a478bb10b12b500d576c44f46be4"],["/archives/index.html","11c8387fd4aeae5b81b91b6b1e91cb9b"],["/categories/Maybe-study/index.html","e7250e14b96eeeddedae04b189a9482e"],["/categories/index.html","aef95663288e7ae22901f32dd0b37e26"],["/comments/index.html","e8064a2815c36765db44faf1635ab1aa"],["/css/APlayer.min.css","fbe994054426fadb2dff69d824c5c67a"],["/css/hbe.style.css","a8bc819e01e001d3bc6ae03a2afad957"],["/css/index.css","852471fa7da19a01ab9499f7cd9129c0"],["/css/personal/custom.css","8a1f051036db45d05f25e59dd5bfacd0"],["/css/personal/picture.css","3d57ebf5aeed343a247a82c156d0fd18"],["/css/personal/progress_bar.css","75ee841f50186cf741ee00392ef3d9a5"],["/css/var.css","d41d8cd98f00b204e9800998ecf8427e"],["/image/avator.jpg","d3c230704a7b42443f4c79473e51c23c"],["/image/banner.jpg","42a3131ea52cd7e1b75da19e4c094f31"],["/image/banner_1.jpg","6c6edbbabf7d3bbc2cf8693af16b6c07"],["/image/browser_tab.jpg","399b493b717418e4c158f36deb0d3849"],["/image/default_cover.jpg","72601a53b17ca7144b4f4b83233fd28c"],["/img/404.jpg","4ef3cfb882b6dd4128da4c8745e9a507"],["/img/favicon.png","7a8c47cb5a2149c1a1af21e90ecd9ca7"],["/img/friend_404.gif","68af0be9d22722e74665ef44dd532ba8"],["/index.html","7a6f4a550eed6ab4f72555c63dcead77"],["/js/APlayer.min.js","8f1017e7a73737e631ff95fa51e4e7d7"],["/js/Meting.min.js","bfac0368480fd344282ec018d28f173d"],["/js/main.js","b28662bf81abe2838d20faf2980f0034"],["/js/personal/runtime.js","ee85158b3ade8e51635f055c0f3b052d"],["/js/search/algolia.js","308de383b932f321e04751fd1f79cffb"],["/js/search/local-search.js","149fcc60c1de0a818e111978d01cbd87"],["/js/tw_cn.js","58cb9443474ea5d84a1c4ac806c8b4b7"],["/js/utils.js","24971090b8b1bd5d3f538d414e270fd3"],["/lib/hbe.js","4a7e94ac7224adfce4b0572ca5c57fef"],["/link/index.html","41de8b0b7b3c54edcad1f43e98a7f96f"],["/movies/index.html","8bfc6b32cb93adb733537461f5e6c9e4"],["/music/index.html","ebeac3f4490d48e042fa856e58f6f8fd"],["/sw-register.js","eb8bcc4182ac053201cf1ff8e2ac6d03"],["/tags/index.html","52effb6d35cb3325773c0d6b77eb94dc"],["/tags/作为日记加密/index.html","e4cf891d825ba2fd91acee6850449aa1"]],cacheName="sw-precache-v3--"+(self.registration?self.registration.scope:""),firstRegister=1,ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,a){var t=new URL(e);return"/"===t.pathname.slice(-1)&&(t.pathname+=a),t.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then((function(a){return new Response(a,{headers:e.headers,status:e.status,statusText:e.statusText})})):Promise.resolve(e)},createCacheKey=function(e,a,t,n){var c=new URL(e);return n&&c.pathname.match(n)||(c.search+=(c.search?"&":"")+encodeURIComponent(a)+"="+encodeURIComponent(t)),c.toString()},isPathWhitelisted=function(e,a){if(0===e.length)return!0;var t=new URL(a).pathname;return e.some((function(e){return t.match(e)}))},stripIgnoredUrlParameters=function(e,a){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map((function(e){return e.split("=")})).filter((function(e){return a.every((function(a){return!a.test(e[0])}))})).map((function(e){return e.join("=")})).join("&"),t.toString()},hashParamName=(addDirectoryIndex=function(e,a){var t=new URL(e);return"/"===t.pathname.slice(-1)&&(t.pathname+=a),t.toString()},"_sw-precache"),urlsToCacheKeys=new Map(precacheConfig.map((function(e){var a=e[0],t=e[1],n=new URL(a,self.location),c=createCacheKey(n,hashParamName,t,!1);return[n.toString(),c]})));function setOfCachedUrls(e){return e.keys().then((function(e){return e&&e.length>0&&(firstRegister=0),e.map((function(e){return e.url}))})).then((function(e){return new Set(e)}))}self.addEventListener("install",(function(e){e.waitUntil(caches.open(cacheName).then((function(e){return setOfCachedUrls(e).then((function(a){return Promise.all(Array.from(urlsToCacheKeys.values()).map((function(t){if(!a.has(t)){var n=new Request(t,{credentials:"same-origin"});return fetch(n).then((function(a){if(!a.ok)throw new Error("Request for "+t+" returned a response with status "+a.status);return cleanResponse(a).then((function(a){return e.put(t,a)}))}))}})))}))})).then((function(){return self.skipWaiting()})))})),self.addEventListener("activate",(function(e){var a=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then((function(e){return e.keys().then((function(t){return Promise.all(t.map((function(t){if(!a.has(t.url))return e.delete(t)})))}))})).then((function(){return self.clients.claim()})).then((function(){if(!firstRegister)return self.clients.matchAll().then((function(e){e&&e.length&&e.forEach((function(e){e.postMessage("sw.update")}))}))})))})),self.addEventListener("fetch",(function(e){if("GET"===e.request.method){var a,t=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching),n="index.html";!(a=urlsToCacheKeys.has(t))&&n&&(t=addDirectoryIndex(t,n),a=urlsToCacheKeys.has(t));0,a&&e.respondWith(caches.open(cacheName).then((function(e){return e.match(urlsToCacheKeys.get(t)).then((function(e){if(e)return e;throw Error("The cached response that was expected is missing.")}))})).catch((function(a){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,a),fetch(e.request)})))}}));
+/**
+ * 自动引入模板，在原有 sw-precache 插件默认模板基础上做的二次开发
+ *
+ * 因为是自定导入的模板，项目一旦生成，不支持随 sw-precache 的版本自动升级。
+ * 可以到 Lavas 官网下载 basic 模板内获取最新模板进行替换
+ *
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var precacheConfig = [["/2022/10/30/Test/index.html","ef63f9ef09a21bc3b3ce5c529fe610e8"],["/2022/10/30/hello-world/index.html","8e759ac3d2564bfe30503bbbcc531667"],["/2022/12/16/Life/index.html","0faf70b5c243121dbbcc9e54fecfe080"],["/2023/04/02/Demo-1/index.html","8d7c19513d390a8f6c3e1c90d0f86867"],["/2023/04/18/Demo-2/index.html","f2d765255e873554bb27a1c5eaab5a77"],["/2023/08/22/23电赛B题/index.html","b1908f4a4547436fbe0733cf1330f1a9"],["/404.html","9df3288d4bf066a658dd2fea48697421"],["/about/index-1.html","967b254eba7c3de5ced4370fc68e3527"],["/about/index.html","0f4db2506ccee8074cedc1068e53cd21"],["/archives/2022/10/index.html","c988551d1152b41b711a1b0dde114442"],["/archives/2022/12/index.html","155bf61af64b514754aae24f8f6876cd"],["/archives/2022/index.html","46e7e24056a6ede2cf33e797f6491f91"],["/archives/2023/04/index.html","e7435dc3b9afa400b6504d80eb4fe58c"],["/archives/2023/08/index.html","1dab10a9ce2f043cfcda977f8d8bf3ca"],["/archives/2023/index.html","e8cb5eee556eb0ced2d01aee07053b26"],["/archives/index.html","527e0c743f81418fc2601b82d5b59117"],["/categories/Maybe-study/index.html","be3c9c581e283b7aa00d092525737534"],["/categories/index.html","ac573cec487446086cac38f25be46a0d"],["/comments/index.html","75b9cd2eedb545419650f309a60093ad"],["/css/APlayer.min.css","e1fbeb84b889055ca44f8cfbe96111b7"],["/css/hbe.style.css","0e364acc009058cdc3674b3f6c7e89e5"],["/css/index.css","b65dc651a67bddbfd986317e16c93a08"],["/css/personal/custom.css","8453b8bec9dfa0f49091e4ba0b74610e"],["/css/personal/picture.css","5de087225e0e9eaa08016a2b151ab570"],["/css/personal/progress_bar.css","960b4844202d6ce589cd4eb2c2bd11bb"],["/css/var.css","d41d8cd98f00b204e9800998ecf8427e"],["/image/avator.jpg","d3c230704a7b42443f4c79473e51c23c"],["/image/banner.jpg","42a3131ea52cd7e1b75da19e4c094f31"],["/image/banner_1.jpg","6c6edbbabf7d3bbc2cf8693af16b6c07"],["/image/browser_tab.jpg","399b493b717418e4c158f36deb0d3849"],["/image/default_cover.jpg","72601a53b17ca7144b4f4b83233fd28c"],["/img/404.jpg","4ef3cfb882b6dd4128da4c8745e9a507"],["/img/favicon.png","7a8c47cb5a2149c1a1af21e90ecd9ca7"],["/img/friend_404.gif","68af0be9d22722e74665ef44dd532ba8"],["/index.html","da91fd3a1ab01397b687b11d6d8b6b9e"],["/js/APlayer.min.js","8f1017e7a73737e631ff95fa51e4e7d7"],["/js/Meting.min.js","bfac0368480fd344282ec018d28f173d"],["/js/main.js","c0b348c16e2170bd8d6dedecdec5d7cd"],["/js/personal/runtime.js","693c84f87c99a4705de01a42f404f8e8"],["/js/search/algolia.js","490ac3474717299789d089b966a95d33"],["/js/search/local-search.js","9a3fa909a665a2ea5c2717e91d4961bb"],["/js/tw_cn.js","4e8419a2b011e14a238a8d2b77a1268d"],["/js/utils.js","30eb8ecce3a0f0e0e67582c7a589212b"],["/lib/hbe.js","bbf3e394ae37f1ddc8f1095f78a1eac4"],["/link/index.html","69536b347aae9a733c820aff324e0467"],["/movies/index.html","bf9323eec70498226e49e5877f445df8"],["/music/index.html","a508181cbab11ccab62603cedbe66bcf"],["/sw-register.js","ecca6566fdbb0d9d07c40b047aa5104c"],["/tags/index.html","76750bd79b3bacec1f12ee18064d9500"],["/tags/作为日记加密/index.html","c6c4678115e32d9f6782c46d56e3bfcf"]];
+var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
+var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
+
+
+var ignoreUrlParametersMatching = [/^utm_/];
+
+
+var addDirectoryIndex = function (originalUrl, index) {
+    var url = new URL(originalUrl);
+    if (url.pathname.slice(-1) === '/') {
+        url.pathname += index;
+    }
+    return url.toString();
+};
+
+var cleanResponse = function (originalResponse) {
+    // 如果没有重定向响应，不需干啥
+    if (!originalResponse.redirected) {
+        return Promise.resolve(originalResponse);
+    }
+
+    // Firefox 50 及以下不知处 Response.body 流, 所以我们需要读取整个body以blob形式返回。
+    var bodyPromise = 'body' in originalResponse ?
+        Promise.resolve(originalResponse.body) :
+        originalResponse.blob();
+
+    return bodyPromise.then(function (body) {
+        // new Response() 可同时支持 stream or Blob.
+        return new Response(body, {
+            headers: originalResponse.headers,
+            status: originalResponse.status,
+            statusText: originalResponse.statusText
+        });
+    });
+};
+
+var createCacheKey = function (originalUrl, paramName, paramValue,
+    dontCacheBustUrlsMatching) {
+
+    // 创建一个新的URL对象，避免影响原始URL
+    var url = new URL(originalUrl);
+
+    // 如果 dontCacheBustUrlsMatching 值没有设置，或是没有匹配到，将值拼接到url.serach后
+    if (!dontCacheBustUrlsMatching ||
+        !(url.pathname.match(dontCacheBustUrlsMatching))) {
+        url.search += (url.search ? '&' : '') +
+            encodeURIComponent(paramName) + '=' + encodeURIComponent(paramValue);
+    }
+
+    return url.toString();
+};
+
+var isPathWhitelisted = function (whitelist, absoluteUrlString) {
+    // 如果 whitelist 是空数组，则认为全部都在白名单内
+    if (whitelist.length === 0) {
+        return true;
+    }
+
+    // 否则逐个匹配正则匹配并返回
+    var path = (new URL(absoluteUrlString)).pathname;
+    return whitelist.some(function (whitelistedPathRegex) {
+        return path.match(whitelistedPathRegex);
+    });
+};
+
+var stripIgnoredUrlParameters = function (originalUrl,
+    ignoreUrlParametersMatching) {
+    var url = new URL(originalUrl);
+    // 移除 hash; 查看 https://github.com/GoogleChrome/sw-precache/issues/290
+    url.hash = '';
+
+    url.search = url.search.slice(1) // 是否包含 '?'
+        .split('&') // 分割成数组 'key=value' 的形式
+        .map(function (kv) {
+            return kv.split('='); // 分割每个 'key=value' 字符串成 [key, value] 形式
+        })
+        .filter(function (kv) {
+            return ignoreUrlParametersMatching.every(function (ignoredRegex) {
+                return !ignoredRegex.test(kv[0]); // 如果 key 没有匹配到任何忽略参数正则，就 Return true
+            });
+        })
+        .map(function (kv) {
+            return kv.join('='); // 重新把 [key, value] 格式转换为 'key=value' 字符串
+        })
+        .join('&'); // 将所有参数 'key=value' 以 '&' 拼接
+
+    return url.toString();
+};
+
+
+var addDirectoryIndex = function (originalUrl, index) {
+    var url = new URL(originalUrl);
+    if (url.pathname.slice(-1) === '/') {
+        url.pathname += index;
+    }
+    return url.toString();
+};
+
+var hashParamName = '_sw-precache';
+var urlsToCacheKeys = new Map(
+    precacheConfig.map(function (item) {
+        var relativeUrl = item[0];
+        var hash = item[1];
+        var absoluteUrl = new URL(relativeUrl, self.location);
+        var cacheKey = createCacheKey(absoluteUrl, hashParamName, hash, false);
+        return [absoluteUrl.toString(), cacheKey];
+    })
+);
+
+function setOfCachedUrls(cache) {
+    return cache.keys().then(function (requests) {
+        // 如果原cacheName中没有缓存任何收，就默认是首次安装，否则认为是SW更新
+        if (requests && requests.length > 0) {
+            firstRegister = 0; // SW更新
+        }
+        return requests.map(function (request) {
+            return request.url;
+        });
+    }).then(function (urls) {
+        return new Set(urls);
+    });
+}
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return setOfCachedUrls(cache).then(function (cachedUrls) {
+                return Promise.all(
+                    Array.from(urlsToCacheKeys.values()).map(function (cacheKey) {
+                        // 如果缓存中没有匹配到cacheKey，添加进去
+                        if (!cachedUrls.has(cacheKey)) {
+                            var request = new Request(cacheKey, { credentials: 'same-origin' });
+                            return fetch(request).then(function (response) {
+                                // 只要返回200才能继续，否则直接抛错
+                                if (!response.ok) {
+                                    throw new Error('Request for ' + cacheKey + ' returned a ' +
+                                        'response with status ' + response.status);
+                                }
+
+                                return cleanResponse(response).then(function (responseToCache) {
+                                    return cache.put(cacheKey, responseToCache);
+                                });
+                            });
+                        }
+                    })
+                );
+            });
+        })
+            .then(function () {
+            
+            // 强制 SW 状态 installing -> activate
+            return self.skipWaiting();
+            
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    var setOfExpectedUrls = new Set(urlsToCacheKeys.values());
+
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.keys().then(function (existingRequests) {
+                return Promise.all(
+                    existingRequests.map(function (existingRequest) {
+                        // 删除原缓存中相同键值内容
+                        if (!setOfExpectedUrls.has(existingRequest.url)) {
+                            return cache.delete(existingRequest);
+                        }
+                    })
+                );
+            });
+        }).then(function () {
+            
+            return self.clients.claim();
+            
+        }).then(function () {
+                // 如果是首次安装 SW 时, 不发送更新消息（是否是首次安装，通过指定cacheName 中是否有缓存信息判断）
+                // 如果不是首次安装，则是内容有更新，需要通知页面重载更新
+                if (!firstRegister) {
+                    return self.clients.matchAll()
+                        .then(function (clients) {
+                            if (clients && clients.length) {
+                                clients.forEach(function (client) {
+                                    client.postMessage('sw.update');
+                                })
+                            }
+                        })
+                }
+            })
+    );
+});
+
+
+
+    self.addEventListener('fetch', function (event) {
+        if (event.request.method === 'GET') {
+
+            // 是否应该 event.respondWith()，需要我们逐步的判断
+            // 而且也方便了后期做特殊的特殊
+            var shouldRespond;
+
+
+            // 首先去除已配置的忽略参数及hash
+            // 查看缓存简直中是否包含该请求，包含就将shouldRespond 设为true
+            var url = stripIgnoredUrlParameters(event.request.url, ignoreUrlParametersMatching);
+            shouldRespond = urlsToCacheKeys.has(url);
+
+            // 如果 shouldRespond 是 false, 我们在url后默认增加 'index.html'
+            // (或者是你在配置文件中自行配置的 directoryIndex 参数值)，继续查找缓存列表
+            var directoryIndex = 'index.html';
+            if (!shouldRespond && directoryIndex) {
+                url = addDirectoryIndex(url, directoryIndex);
+                shouldRespond = urlsToCacheKeys.has(url);
+            }
+
+            // 如果 shouldRespond 仍是 false，检查是否是navigation
+            // request， 如果是的话，判断是否能与 navigateFallbackWhitelist 正则列表匹配
+            var navigateFallback = '';
+            if (!shouldRespond &&
+                navigateFallback &&
+                (event.request.mode === 'navigate') &&
+                isPathWhitelisted([], event.request.url)
+            ) {
+                url = new URL(navigateFallback, self.location).toString();
+                shouldRespond = urlsToCacheKeys.has(url);
+            }
+
+            // 如果 shouldRespond 被置为 true
+            // 则 event.respondWith()匹配缓存返回结果，匹配不成就直接请求.
+            if (shouldRespond) {
+                event.respondWith(
+                    caches.open(cacheName).then(function (cache) {
+                        return cache.match(urlsToCacheKeys.get(url)).then(function (response) {
+                            if (response) {
+                                return response;
+                            }
+                            throw Error('The cached response that was expected is missing.');
+                        });
+                    }).catch(function (e) {
+                        // 如果捕获到异常错误，直接返回 fetch() 请求资源
+                        console.warn('Couldn\'t serve response for "%s" from cache: %O', event.request.url, e);
+                        return fetch(event.request);
+                    })
+                );
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+/* eslint-enable */
